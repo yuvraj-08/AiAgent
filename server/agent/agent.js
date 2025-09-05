f346a34ec294fea3750401dcb7f6b483201eee5b
Yuvraj Singh <contact.yuvraj08@gmail.com>
2025-09-05 17:52:31 +0530

    initiated adding tools

--- server/agent/agent.js
+++ server/agent/agent.js
@@ -1,22 +1,51 @@
 import { createReactAgent } from "@langchain/langgraph/prebuilt";
 import { ChatMistralAI } from "@langchain/mistralai";
+import { tool } from "@langchain/core/tools";
+import { z } from "zod";
+import { MemorySaver } from "@langchain/langgraph";
 
+// creating a tool which will be consumed by agent.
+// tool gives additional power to agents
+// !tool's 1st param -> a function
+// !tool's 2nd param -> configuration
+const weatherTool = tool(
+  async ({ query }) => {
+    console.log("query", query);
+
+    // TODO: Implement the weather tool by fetching an API
+
+    return "The weather in punjab is floody";
+  },
+  {
+    name: "weather", // name of the tool
+    description: "Get the weather in a given location", // tells agent when to use this tool
+    // schema to tell agent how to call this tool
+    schema: z.object({
+      query: z.string().describe("The query to use in search"),
+    }),
+  }
+);
+
+// autopicks the API key from the .env file
 const model = new ChatMistralAI({
-  model: "mistral-small-latest",
+  model: "mistral-small-latest", // mistral free version model, auto points to latest version
 });
 
+// creats agent which is later used to make requests.
 const agent = createReactAgent({
   llm: model,
-  tools: [],
+  tools: [weatherTool],
 });
 
+// invoke makes a call to the llm model
 const result = await agent.invoke({
   messages: [
     {
       role: "user",
-      content: "Hello, how can you help me?",
+      content: "Whats the weather in punjab",
     },
   ],
 });
 
+// logs out the result received from the agent.
 console.log(result?.messages.at(-1)?.content);
