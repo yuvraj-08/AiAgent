3bbaa0f91c45f6df25c98e44669e2ad5f0771a36
Yuvraj Singh <contact.yuvraj08@gmail.com>
2025-09-06 14:37:28 +0530

    added JsExecutor

--- server/agent/agent.js
+++ server/agent/agent.js
@@ -26,6 +26,46 @@ const weatherTool = tool(
   }
 );
 
+async function evalAndCaptureOutput(code) {
+  const oldLog = console.log;
+  const oldError = console.error;
+
+  const output = [];
+  let errorOutput = [];
+
+  console.log = (...args) => output.push(args.join(" "));
+  console.error = (...args) => errorOutput.push(args.join(" "));
+
+  try {
+    await eval(code);
+  } catch (error) {
+    errorOutput.push(error.message);
+  }
+
+  console.log = oldLog;
+  console.error = oldError;
+
+  return { stdout: output.join("\n"), stderr: errorOutput.join("\n") };
+}
+
+const jsExecutor = tool(
+  async ({ code }) => {
+    const result = await evalAndCaptureOutput(code);
+    return result;
+  },
+  {
+    name: "run_javascript_code_tool",
+    description: `
+      Run general purpose javascript code. 
+      This can be used to access Internet or do any computation that you need. 
+      The output will be composed of the stdout and stderr. 
+      The code should be written in a way that it can be executed with javascript eval in node environment.
+    `,
+    schema: z.object({
+      code: z.string().describe("code to be executed"),
+    }),
+  }
+);
 // autopicks the API key from the .env file
 const model = new ChatMistralAI({
   model: "mistral-small-latest", // mistral free version model, auto points to latest version
@@ -37,7 +77,7 @@ const checkpointSaver = new MemorySaver();
 // creats agent which is later used to make requests.
 export const agent = createReactAgent({
   llm: model,
-  tools: [weatherTool],
+  tools: [weatherTool, jsExecutor],
   checkpointSaver, // pass memory instance to agent
 });
 
