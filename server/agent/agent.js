1d592b010346d079053fa8b018afe149a54a00ae
Yuvraj Singh <contact.yuvraj08@gmail.com>
2025-09-06 13:27:11 +0530

    added memory

--- server/agent/agent.js
+++ server/agent/agent.js
@@ -2,7 +2,7 @@ import { createReactAgent } from "@langchain/langgraph/prebuilt";
 import { ChatMistralAI } from "@langchain/mistralai";
 import { tool } from "@langchain/core/tools";
 import { z } from "zod";
-import { MemorySaver } from "@langchain/langgraph";
+import { MemorySaver } from "@langchain/langgraph"; // memory
 
 // creating a tool which will be consumed by agent.
 // tool gives additional power to agents
@@ -31,21 +31,49 @@ const model = new ChatMistralAI({
   model: "mistral-small-latest", // mistral free version model, auto points to latest version
 });
 
+// create memory saver instance
+const checkpointSaver = new MemorySaver();
+
 // creats agent which is later used to make requests.
 const agent = createReactAgent({
   llm: model,
   tools: [weatherTool],
+  checkpointSaver, // pass memory instance to agent
 });
 
 // invoke makes a call to the llm model
-const result = await agent.invoke({
-  messages: [
-    {
-      role: "user",
-      content: "Whats the weather in punjab",
+const result = await agent.invoke(
+  {
+    messages: [
+      {
+        role: "user",
+        content: "Whats the weather in punjab",
+      },
+    ],
+  },
+  {
+    configurable: {
+      thread_id: 42, // tells the agent to store the messages in threa_id 42 for further reference.
     },
-  ],
-});
+  }
+);
+
+const followUp = await agent.invoke(
+  {
+    messages: [
+      {
+        role: "user",
+        content: "Whats city is that for?",
+      },
+    ],
+  },
+  {
+    configurable: {
+      thread_id: 42, // passing same thread to make reference to the previous chat
+    },
+  }
+);
 
 // logs out the result received from the agent.
 console.log(result?.messages.at(-1)?.content);
+console.log(followUp?.messages.at(-1)?.content);
